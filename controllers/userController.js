d8553c23aac6a19c9ac077c8dcaa8f6111f69cd3
ElyaBOrbach <elyaorbach@gmail.com>
2024-12-28 19:34:18 +1100

    Changed success messages and added status codes to responses

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,32 +4,32 @@ const db = require('../models/user');
 
 const postUser = async (req, res) => {
     const { username, password } = req.body;
-    if (!username || !password) return res.json({ statusCode: 400, message: "Username or password is missing" });
+    if (!username || !password) return res.status(400).json({ statusCode: 400, message: "Username or password is missing" });
 
     const hash = await bcrypt.hash(password, 10);
     const userToAdd = { username, password: hash };
     db.postUser(userToAdd, (error, result) => {
         if (!error) {
-            res.json({ statusCode:201,data:result,message:'New user registered'});
+            res.status(201).json({ statusCode:201,data:result,message:'New user registered'});
         }
         else if (error.message == "User already exists"){
-            res.json({ statusCode:409, message:'User already exists'});
+            res.status(409).json({ statusCode:409, message:'User already exists'});
         }
     });
 };
 
 const login = async (req, res) => {
     const { username, password } = req.body;
-    if (!username || !password) return res.json({ statusCode: 400, message: "Username or password is missing" });
+    if (!username || !password) return res.status(400).json({ statusCode: 400, message: "Username or password is missing" });
 
     db.getUser(username, async (error, user) => {
-        if (!user) return res.json({ statusCode: 401, message: "Password or Username are incorrect" });
+        if (!user) return res.status(401).json({ statusCode: 401, message: "Password or Username are incorrect" });
 
         const isPassword = await bcrypt.compare(password, user.password);
-        if (!isPassword) return res.json({ statusCode: 401, message: "Password or Username are incorrect" });
+        if (!isPassword) return res.status(401).json({ statusCode: 401, message: "Password or Username are incorrect" });
 
         const token = jwt.sign({ username: user.username }, "temporary_secret_key", { expiresIn: "1h" });
-        res.json({ statusCode: 200, message: "Successfully logged in", token: token });
+        res.status(200).json({ statusCode: 200, message: "Successfully logged in", token: token });
     });
 };
 
@@ -37,10 +37,10 @@ const getUser = async (req, res) => {
     let username = req.user.username;
 
     db.getUser(username, (error, result) => {
-        if(!result) return res.json({ statusCode: 404, message: "User not found" });
+        if(!result) return res.status(404).json({ statusCode: 404, message: "User not found" });
         if (!error){
             delete result.password; // The password retrieved from the database will be a hash so it will be useless to show it
-            res.json({statusCode:200,data:result,message:'success'});
+            res.status(200).json({statusCode:200,data:result,message:'success'});
         }
     });
 };
@@ -53,10 +53,10 @@ async function updateUserWord(req, res) {
 
     db.updateUserWord(username, category, word, (error) => {
         if (!error){
-            res.json({statusCode:201,message:'success'});
+            res.status(201).json({statusCode:201,message:'Word updated'});
         }
         else if(error.message == "User does not exist"){
-            res.json({statusCode:404,message:"User does not exist"});
+            res.status(404).json({statusCode:404,message:"User does not exist"});
         }
     })
 }
@@ -67,10 +67,10 @@ async function updateUserPassword(req, res) {
     const hash = await bcrypt.hash(password, 10);
     db.updateUserPassword(username, hash, (error) => {
         if (!error){
-            res.json({statusCode:201,message:'success'});
+            res.status(201).json({statusCode:201,message:'Password updated'});
         }
         else if(error.message == "User does not exist"){
-            res.json({statusCode:404,message:"User does not exist"});
+            res.status(404).json({statusCode:404,message:"User does not exist"});
         }
     })
 }
@@ -80,7 +80,7 @@ async function deleteUser(req, res) {
 
     db.deleteUser(username, (error) => {
         if (!error){
-            res.json({statusCode:204,message:'success'});
+            res.status(204).json({statusCode:204,message:'User deleted'});
         }
     })
 }
