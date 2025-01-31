3920d4984b9a9fc94cfd58c6fff7a56470b6ae6c
ElyaBOrbach <elyaorbach@gmail.com>
2025-01-31 14:42:38 +1100

    Fixed broken tests

--- test/word.test.js
+++ test/word.test.js
@@ -86,15 +86,15 @@ describe('Testing Word Routes', () => {
         server.close();
     });
 
-    describe('GET api/word/:category', () => {
+    describe('GET api/word', () => {
         it('should return a list of all words in that category if it exists', async () => {
-            const response = await request(server).get('/api/word/domesticated_animals');
+            const response = await request(server).get('/api/word?categories=domesticated_animals');
             expect(response.status).toBe(200);
-            expect(response.body.data.length).toEqual(4);
-            expect(response.body.message).toBe('Word list successfully retrieved');
+            expect(response.body.data.domesticated_animals.length).toEqual(4);
+            expect(response.body.message).toBe('Word lists successfully retrieved');
         });
         it('should respond with an 404 error if the category does not exist', async () => {
-            const response = await request(server).get('/api/word/animals');
+            const response = await request(server).get('/api/word?categories=animals');
             expect(response.status).toBe(404);
             expect(response.body.message).toBe('Category not found');
         });
