a97237b05343842e12f385384146696d6954e3f4
ElyaBOrbach <elyaorbach@gmail.com>
2025-02-01 10:57:38 +1100

    made final endpoint tests pass

--- test/gameDataRoutesTests.js
+++ test/gameDataRoutesTests.js
@@ -6,33 +6,33 @@ module.exports = (server) => {
             server.close();
         });
 
-        // describe('GET api/game/categories/:number', () => {
-        //     it('should respond with a 404 error if no number is given', async () => {
-        //         const response = await request(server).get('/api/game/categories');
-        //         expect(response.status).toBe(404);
-        //     });
-        //     it('should respond with a 400 error if number is not a number is given', async () => {
-        //         const response = await request(server).get('/api/game/categories/num');
-        //         expect(response.status).toBe(400);
-        //     });
-        //     it('should respond with a 200 if categories are successfully retrieved', async () => {
-        //         const response = await request(server).get('/api/game/categories/3');
-        //         expect(response.status).toBe(200);
-        //         expect(response.body.data.length).toBe(3);
-        //     });
-        // });
+        describe('GET api/game/categories/:number', () => {
+            it('should respond with a 404 error if no number is given', async () => {
+                const response = await request(server).get('/api/game/categories');
+                expect(response.status).toBe(404);
+            });
+            it('should respond with a 400 error if number is not a number is given', async () => {
+                const response = await request(server).get('/api/game/categories/num');
+                expect(response.status).toBe(400);
+            });
+            it('should respond with a 200 if categories are successfully retrieved', async () => {
+                const response = await request(server).get('/api/game/categories/3');
+                expect(response.status).toBe(200);
+                expect(response.body.data.length).toBe(3);
+            });
+        });
 
-        // describe('GET api/game/categories/:number/:game', () => {
-        //     it('should respond with a 400 error if number is not a number is given', async () => {
-        //         const response = await request(server).get('/api/game/categories/num/History');
-        //         expect(response.status).toBe(400);
-        //     });
-        //     it('should respond with a 200 if categories are successfully retrieved', async () => {
-        //         const response = await request(server).get('/api/game/categories/3/History');
-        //         expect(response.status).toBe(200);
-        //         expect(response.body.data.length).toBe(2); // only responds with 2 since there is only 2 history categories
-        //     });
-        // });
+        describe('GET api/game/categories/:number/:game', () => {
+            it('should respond with a 400 error if number is not a number is given', async () => {
+                const response = await request(server).get('/api/game/categories/num/History');
+                expect(response.status).toBe(400);
+            });
+            it('should respond with a 200 if categories are successfully retrieved', async () => {
+                const response = await request(server).get('/api/game/categories/3/History');
+                expect(response.status).toBe(200);
+                expect(response.body.data.length).toBe(3);
+            });
+        });
 
         describe('GET api/game/players/:number', () => {
             it('should return with a 400 error if categories are not given', async () => {
